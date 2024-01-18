8028fed65654c73bf3cf49a937f7c2664671fae9
Sergio Azizi <sergioazizi@Sergios-MacBook-Pro.local>
2024-01-18 01:03:25 +0000

    candlestick chart

---
+++ CandleStickPlot.tsx
@@ -0,0 +1,111 @@
+import styled from "styled-components";
+import { CandleWithSMA, Trade } from "./types/types";
+import Chart from "react-apexcharts";
+
+export const CandlePlot = ({
+  candles,
+  trades,
+}: {
+  candles: Array<CandleWithSMA>;
+  trades: Array<Trade>;
+}) => {
+  if (!candles.length || !trades.length) return null;
+
+  return (
+    <ChartWrapper>
+      <Chart
+        // width={candles.length * 5}
+        height={365}
+        series={[
+          {
+            name: "candle",
+            type: "candlestick",
+            data: candles.map(({ time, o, h, l, c }) => {
+              // return [Date.parse(time), [o, h, l, c]];
+              return { x: Date.parse(time), y: [o, h, l, c] };
+            }),
+          },
+          {
+            name: "sma",
+            type: "line",
+            data: candles.map((candle) => {
+              return { x: Date.parse(candle.time), y: candle.sma };
+              // return [Date.parse(candle.time), candle.sma];
+            }),
+          },
+        ]}
+        type="candlestick"
+        options={{
+          annotations: {
+            xaxis: trades.map(({ entryTime, exitTime, profitLoss }) => {
+              return {
+                x: new Date(entryTime).getTime(),
+                x2: new Date(exitTime).getTime(),
+                fillColor: profitLoss > 0 ? "#00ff0060" : "#ff000060",
+                borderColor: profitLoss > 0 ? "#00ff00" : "#ff0000",
+                strokeDashArray: 0,
+              };
+            }),
+          },
+          plotOptions: {
+            bar: {
+              columnWidth: "150%",
+            },
+          },
+          title: {
+            text: "EUR/GBP 2023",
+            align: "left",
+          },
+          stroke: {
+            curve: "straight",
+          },
+          chart: {
+            width: "100%",
+            events: {
+              beforeZoom: function (ctx: any) {
+                // we need to clear the range as we only need it on the iniital load.
+                ctx.w.config.xaxis.range = undefined;
+              },
+              mouseMove: function (ev: any, ctx: any) {
+                // we need to clear the range as we only need it on the iniital load.
+                ctx.w.config.xaxis.range = undefined;
+              },
+            },
+          },
+          xaxis: {
+            // type: "datetime",
+            type: "category",
+            // type: "numeric",
+            range:
+              new Date(
+                candles[candles.length > 50 ? 50 : candles.length - 1].time
+              ).getTime() - new Date(candles[0].time).getTime(), // for inital zoom
+            tickPlacement: "on",
+            tickAmount: 19,
+            labels: {
+              formatter: (value: any, timestamp: any, opts: any) => {
+                var d = new Date(value),
+                  month = "" + (d.getMonth() + 1),
+                  day = "" + d.getDate(),
+                  year = d.getFullYear();
+
+                if (month.length < 2) month = "0" + month;
+                if (day.length < 2) day = "0" + day;
+
+                return [year, month, day].join("/");
+              },
+            },
+          },
+        }}
+      />
+    </ChartWrapper>
+  );
+};
+
+const ChartWrapper = styled.div`
+  border: 1px solid black;
+  width: 80%;
+  height: fit-content;
+  /* overflow-x: au; */
+  position: relative;
+`;
