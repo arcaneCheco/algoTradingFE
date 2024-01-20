281637290329881828d50718de8447bb3c5d03b4
Sergio Azizi <sergioazizi@Sergios-MacBook-Pro.local>
2024-01-20 00:15:11 +0000

    feat: use shared types repo

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,30 +3,6 @@ export enum PricingComponent {
   B = "B",
   A = "A",
 }
-export enum CandlestickGranularity {
-  S5 = "S5",
-  S10 = "S10",
-  S15 = "S15",
-  S30 = "S30",
-  M1 = "M1",
-  M2 = "M2",
-  M3 = "M3",
-  M4 = "M4",
-  M5 = "M5",
-  M10 = "M10",
-  M15 = "M15",
-  M30 = "M30",
-  H1 = "H1",
-  H2 = "H2",
-  H3 = "H3",
-  H4 = "H4",
-  H6 = "H6",
-  H8 = "H8",
-  H12 = "H12",
-  D = "D",
-  W = "W",
-  M = "M",
-}
 
 export enum Signal {
   BUY = "BUY",
